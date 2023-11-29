8d61fa54ad3cba0697518bffe98defea23863337
mattam <67870103+mattam@users.noreply.github.com>
2023-11-29 12:50:30 +0100

    tried debugging

--- Core/Src/task_GPS.c
+++ Core/Src/task_GPS.c
@@ -9,14 +9,14 @@
 
 extern UART_HandleTypeDef GPS_UART;
 
-static HAL_StatusTypeDef gps_rcv_line(char *buffer, uint16_t length)
+static HAL_StatusTypeDef gps_rcv_line(char *buffer, uint16_t length, uint32_t timeout)
 {
     size_t i = 0;
     HAL_StatusTypeDef ret;
 
     for (i = 0; i < length; i++)
     {
-        ret = HAL_UART_Receive(&GPS_UART, (uint8_t *)&buffer[i], 1, HAL_MAX_DELAY);
+        ret = HAL_UART_Receive(&GPS_UART, (uint8_t *)&buffer[i], 1, timeout);
 
         if (buffer[i] == '\n' || ret != HAL_OK)
         {
@@ -30,10 +30,10 @@ static HAL_StatusTypeDef gps_rcv_line(char *buffer, uint16_t length)
 size_t get_len_of_cmd(const char *str)
 {
 
-	uint8_t i = 0;
-    	while(i < 128 - 6)
-        {
-    	switch (str[i])
+    uint8_t i = 0;
+    while (i < 128 - 6)
+    {
+        switch (str[i])
         {
         case '\0':
         case '\n':
@@ -43,35 +43,35 @@ size_t get_len_of_cmd(const char *str)
             return i;
         }
         i++;
-        }
+    }
 
     return 0; // Invalid command
 }
 
-static HAL_StatusTypeDef gps_send_line(const char *buffer)
+static HAL_StatusTypeDef gps_send_line(const char *buffer, uint32_t timeout)
 {
-	HAL_StatusTypeDef ret = HAL_OK;
+    HAL_StatusTypeDef ret = HAL_OK;
 
-	size_t cmdlen = get_len_of_cmd(buffer);
-	const line_len = cmdlen + 6;// Buffer + '*'+ Checksum (2 Hex Chars) + CR + LF + null
-	char *outbuf = (char *)malloc(line_len);
-	if (outbuf == NULL)
-	{
-		return HAL_ERROR;
-	}
+    size_t cmdlen = get_len_of_cmd(buffer);
+    const line_len = cmdlen + 6; // Buffer + '*'+ Checksum (2 Hex Chars) + CR + LF + null
+    char *outbuf = (char *)malloc(line_len);
+    if (outbuf == NULL)
+    {
+        return HAL_ERROR;
+    }
 
-	memcpy(outbuf, buffer, cmdlen);
-	outbuf[cmdlen] = '\0';
+    memcpy(outbuf, buffer, cmdlen);
+    outbuf[cmdlen] = '\0';
 
-	uint8_t chck = minmea_checksum(outbuf);
-	sprintf(outbuf + cmdlen, "*%02X\n\r",chck );
+    uint8_t chck = minmea_checksum(outbuf);
+    sprintf(outbuf + cmdlen, "*%02X\n\r", chck);
     if (minmea_check(outbuf, true))
     {
-        ret = HAL_UART_Transmit(&GPS_UART, outbuf, strlen(outbuf),HAL_MAX_DELAY);
+        ret = HAL_UART_Transmit(&GPS_UART, outbuf, strlen(outbuf), timeout);
     }
     else
     {
-    	ret = HAL_ERROR;
+        ret = HAL_ERROR;
     }
 
     free(outbuf);
@@ -79,53 +79,132 @@ static HAL_StatusTypeDef gps_send_line(const char *buffer)
     return ret;
 }
 
+HAL_StatusTypeDef set_normal_mode(void)
+{
+    return HAL_UART_Transmit(&GPS_UART, "$PGKC105,0*37\r\n", 15, 1000);
+}
+HAL_StatusTypeDef get_software_version(void)
+{
+    return HAL_UART_Transmit(&GPS_UART, "$PGKC462*2F\r\n", 15, HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef get_nmea_intervall(void)
+{
+    char line[128] = {};
+
+    HAL_StatusTypeDef ret = HAL_UART_Transmit(&GPS_UART, "$PGKC201*2C\r\n", 17, 1000);
+
+    if (ret != HAL_OK)
+    {
+        printf("req failed\n");
+        return HAL_ERROR;
+    }
+
+    ret = gps_rcv_line(line, sizeof(line), 10);
+
+    if (ret != HAL_OK)
+    {
+        printf("rec failed\n");
+        return HAL_ERROR;
+    }
+
+    if (!minmea_check(line, true))
+
+    {
+        printf("check failed\n");
+        return HAL_ERROR;
+    }
+
+    if (!strncmp(line, "$PGKC202", 8) == 0)
+    {
+        printf("Wrong response\n");
+        return HAL_ERROR;
+    }
+    printf("%s\n", line);
+    return HAL_OK;
+}
+
+
+
 void GPS_task(void *pvParameters)
 {
-    char line[500] = {};
+    char line[128] = {};
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     HAL_StatusTypeDef ret = HAL_OK;
-  // char  cmd []  = "$PGKC463,GOKE9501_1.3_17101100*22\r\n";
-  // HAL_UART_Transmit(&GPS_UART, cmd, sizeof(cmd),HAL_MAX_DELAY);
+    set_normal_mode();
 
+    
+    // char  cmd []  = "$PGKC463,GOKE9501_1.3_17101100*22\r\n";
+    // HAL_UART_Transmit(&GPS_UART, cmd, sizeof(cmd),HAL_MAX_DELAY);
 
-  // ret =gps_send_line("$PGKC105,0"); 
-  // 
-  // ret =gps_send_line("$PGKC242,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0"); // set rcv Interval to 200ms
+    // ret =gps_send_line("$PGKC105,0");
+    //
+    // ret =gps_send_line("$PGKC242,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0"); // set rcv Interval to 200ms
+    // if (ret != HAL_OK)
+    // {
+    // 	printf("line1 failed\n");
+    // }
+
+    // ret =  gps_send_line("$PGKC161,2,50,100"); // set rcv Interval to 200ms
+    //  if (ret != HAL_OK)
+    //  {
+    //  	printf("line2 failed\n");
+    //  }
+    //  ret =  gps_send_line("$PGKC161,2,50,100"); // set rcv Interval to 200ms
+    //   if (ret != HAL_OK)
+    //   {
+    //   	printf("line2 failed\n");
+  // ret = HAL_UART_Transmit(&GPS_UART, "$PGKC030,3,1*2E\r\n", 17, 1000);
   // if (ret != HAL_OK)
   // {
-  // 	printf("line1 failed\n");
+  //     printf("restart failed\n");
   // }
 
-  //ret =  gps_send_line("$PGKC161,2,50,100"); // set rcv Interval to 200ms
+  // ret = HAL_UART_Transmit(&GPS_UART, "$PGKC115,1,0,0,0*2B\r\n", 21, 1000);
   // if (ret != HAL_OK)
   // {
-  // 	printf("line2 failed\n");
+  //     printf("setting GPS failed\n");
   // }
-  // ret =  gps_send_line("$PGKC161,2,50,100"); // set rcv Interval to 200ms
-  //  if (ret != HAL_OK)
-  //  {
-  //  	printf("line2 failed\n");
-  //  }
 
+  // ret = HAL_UART_Transmit(&GPS_UART, "$PGKC105,0*37\r\n", 15, 1000);
+  // if (ret != HAL_OK)
+  // {
+  //     printf("line1 failed\n");
+  // }
+
+  // ret = HAL_UART_Transmit(&GPS_UART, "$PGKC030,3,1*2E\r\n", 17, 1000);
+  // if (ret != HAL_OK)
+  // {
+  //     printf("line2 failed\n");
+  // }
+
+    //   }
+    uint8_t ctr = 0;
     while (1)
     {
-        ret =gps_send_line("$PGKC463");
-        ret =gps_send_line("$PGKC463");
 
         delay_ms(1);
         size_t i = 0;
         HAL_StatusTypeDef ret;
 
-        ret = gps_rcv_line(line, sizeof(line));
+        ret = gps_rcv_line(line, sizeof(line), 500);
 
-        while (ret != HAL_OK)
+        if (ret != HAL_OK)
         {
-            ret = gps_rcv_line(line, sizeof(line));
-        }
+            get_software_version();
 
+            // ret = gps_send_line("$PGKC463",1000);
+            //ret = get_nmea_intervall();
+            continue;
+            //ret = gps_rcv_line(line, sizeof(line), 1);
+            //printf("%s\n", line);
+        }
         printf("%s\n", line);
 
-
+        if (strncmp((char *)line + 3, "TXT", 3) == 0)
+        {
+            printf("\n\n\n\r");
+        }
 
         continue;
         if (strncmp((char *)line + 3, "GGA", 3) == 0)
